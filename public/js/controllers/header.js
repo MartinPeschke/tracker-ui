e4ab444a4d47faa44d488af62d78c0ef67509248
Martin Peschke <martin@hackandcraft.com>
2014-02-04 17:10:30 +0100

    hard page reload on logout, just feels better

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('trackerui.system').controller('HeaderController', ['$scope', '$state', 'StateService', function ($scope, $state, State) {
+angular.module('trackerui.system').controller('HeaderController', ['$scope', '$window', 'StateService', function ($scope, $window, State) {
     $scope.menu = [];
     $scope.state = State;
 
     $scope.logout = function(){
         State.logout();
-        $state.go( 'index' );
+        $window.location.href = '/'
     };
 }]);
