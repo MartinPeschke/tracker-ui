2a3a02574910a4d85dfe8b9e947efd09da8d579a
Martin Peschke <martin@hackandcraft.com>
2014-02-07 14:17:44 +0100

    styling attempts

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('trackerui.system').controller('IndexController', ['$scope', function ($scope) {
-    $scope.title = 'Some d3 stuff';
-    $scope.distance = 180;
-    $scope.radius = 300;
-
     $scope.d3Data = [
         {title: 'Greg', score:12, intersect: 5, id:1},
         {title: 'Ari', score:43, intersect: 10, id:2},
