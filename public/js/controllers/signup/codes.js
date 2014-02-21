6069588fe926cf9d7f82ba8ca1b6c0852c922862
Sinisa <sinisa@hackandcraft.com>
2014-02-21 18:29:50 +0100

    preselect event code

--- public/js/controllers/signup/codes.js
+++ public/js/controllers/signup/codes.js
@@ -5,8 +5,8 @@ angular.module('trackerui.system').controller('SignupCodesController', ['$scope'
         $scope.state = State;
 
         $scope.codeSnippets = [];
-
         $scope.displayEventCodes = function(event){
+            $scope.selected = event;
             $scope.codeSnippets = [];
             for (var i=0; i<$scope.state.account.Platforms.length;i++){
                 var snippet = {};
@@ -15,6 +15,9 @@ angular.module('trackerui.system').controller('SignupCodesController', ['$scope'
                 $scope.codeSnippets.push(snippet);
             }
         };
+        //preselected first event
+        $scope.selected = $scope.state.account.Events[0];
+        $scope.displayEventCodes($scope.selected);
 
         $scope.getTextToCopy = function(code){
             return code;
